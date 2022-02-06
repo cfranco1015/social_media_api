bee27ade9303a95da60d82f4b44e5ae5d4cbeaf3
Christian <christian@christians-thinkpad.home>
2022-02-06 15:24:28 -0500

    qwer

--- app/database.py
+++ app/database.py
@@ -9,7 +9,7 @@ from app.config import settings
 #SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
 
 # Testing Database
-SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/test_{settings.database_name}'
+SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
